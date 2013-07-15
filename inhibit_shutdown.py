ab8fb39dd4560834ba7652e345bb4435ca7613e9
rlKoekie <hansvschoot@gmail.com>
2013-07-15 15:03:54 +0200

    added idle delay counter (untested). this will not disable the shutdowninhibitor until the counter is empty.

--- inhibit_shutdown.py
+++ inhibit_shutdown.py
@@ -19,15 +19,15 @@ def port_set(string):
 def log(msg):
     print("{}: {}".format(addon.getAddonInfo('id'), msg))
 
-def check_services():
+def check_services(watchlocal, watchremote):
     """ Check if any of the watched services is running. """
 
     netstat = subprocess.check_output(['/bin/netstat', '-t', '-n'], universal_newlines=True)
 
     for line in netstat.split('\n')[2:]:
         items = line.split()
-        if len(items) < 4:
-            continue
+        if len(items) < 4: continue
+        if not ("udp" in items[0] or "tcp" in items[0] or "raw" in items[0]): continue
 
         local_addr, local_port = items[3].rsplit(':', 1)
         remote_addr, remote_port = items[4].rsplit(':', 1)
@@ -40,8 +40,8 @@ def check_services():
 
         local_port = int(local_port)
 
-        if ((local_addr != remote_addr) and (local_port in watched_remote)) or \
-            ((local_addr == remote_addr) and (local_port in watched_local)):
+        if ((local_addr != remote_addr) and (local_port in watchremote)) or \
+            ((local_addr == remote_addr) and (local_port in watclocal)):
             log("Found connection from {} to {}:{}".format(remote_addr, local_addr, local_port))
             return True
 
@@ -57,6 +57,9 @@ except ValueError:
     sleep_time = 60 * 1000
 watched_local = port_set(s('localports'))
 watched_remote = port_set(s('remoteports'))
+# useless double conversion here, but prevents slightly against idiot input
+counterdefault = int(float(s('idlecount')))
+counter = counterdefault
 
 log("Watching for remote connections to ports {} and for local connections to ports {}, sleep time is {:0.2f} s.".format(
     ', '.join(str(x) for x in watched_remote),
@@ -64,8 +67,13 @@ log("Watching for remote connections to ports {} and for local connections to po
     sleep_time / 1000.0))
 
 while not xbmc.abortRequested:
-    if check_services():
+    if check_services(watched_local, watched_remote):
         xbmc.executebuiltin('InhibitIdleShutdown(true)')
+        counter = counterdefault
+    elif counter > 0: # if we have a value on the counter, we leave shutdown as it is.
+        counter = counter -1
+        log("Delay counter is: %i. Not changing InhibitIdleShutdown.")
     else:
+        log("Setting InhibitIdleShutdown to false")
         xbmc.executebuiltin('InhibitIdleShutdown(false)')
     xbmc.sleep(sleep_time)
