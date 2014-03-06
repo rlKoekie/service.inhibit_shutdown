934f92f048bd30940032c919c58d21ee6e473407
Hans van Schoot <koekie@hans-i7>
2014-03-06 08:50:04 +0100

    A lot of changes. Fixed xbmc shutdown delay, added transmission torrents monitor, added lockfile support, added an option to set a delay before allowing the shutdown again.

--- inhibit_shutdown.py
+++ inhibit_shutdown.py
@@ -5,6 +5,9 @@ import xbmc
 import xbmcaddon
 import subprocess
 import re
+from os import path, popen
+global sleep_time, watched_local, watched_remote, counterdefault, addon
+global lockfilepaths, transmisionMinimalSpeed , transmissionCmd, debugMe
 
 def port_set(string):
     ret = set()
@@ -16,8 +19,9 @@ def port_set(string):
         ret.add(port)
     return ret
 
-def log(msg):
-    print("{}: {}".format(addon.getAddonInfo('id'), msg))
+def mylog(msg):
+    if debugMe:
+        print("{}: {}".format(addon.getAddonInfo('id'), msg))
 
 def check_services(watchlocal, watchremote):
     """ Check if any of the watched services is running. """
@@ -41,39 +45,103 @@ def check_services(watchlocal, watchremote):
         local_port = int(local_port)
 
         if ((local_addr != remote_addr) and (local_port in watchremote)) or \
-            ((local_addr == remote_addr) and (local_port in watclocal)):
-            log("Found connection from {} to {}:{}".format(remote_addr, local_addr, local_port))
+            ((local_addr == remote_addr) and (local_port in watchlocal)):
+            mylog("Found connection from {} to {}:{}".format(remote_addr, local_addr, local_port))
             return True
 
-    log("No connection found.")
+    mylog("No connection found.")
     return False
 
+def check_lockfiles(lockfilelist=[]):
+    for i in lockfilelist:
+        if path.exists(i):
+            mylog("lockfile(s) found: %s" %(i))
+            return True
+    return False
 
-addon = xbmcaddon.Addon()
-s = addon.getSetting
-try:
-    sleep_time = int(float(s('sleep')) * 1000)
-except ValueError:
-    sleep_time = 60 * 1000
-watched_local = port_set(s('localports'))
-watched_remote = port_set(s('remoteports'))
-# useless double conversion here, but prevents slightly against idiot input
-counterdefault = int(float(s('idlecount')))
+# function to check the status of transmission. if running, it checks if the downloads are going fast enough to keep the system awake
+def check_transmission(transmissioncommand,transmissionminimalspeed=10.0):
+    try: transmissioninfo = popen(transmissioncommand).read()
+    except:
+        mylog("transmission-remote error. is transmission-remote installed on this system?")
+        return False
+    else: 
+        if transmissioninfo == '':
+            mylog("transmission not running or not giving any response")
+            return False
+        elif float(transmissioninfo.split()[-1]) >= transmissionminimalspeed:
+            mylog("transmission downloading: %s kb/s" %(transmissioninfo.split()[-1]))
+            return True
+        else:
+            mylog("transmission downloading too slow: %s kb/s" %(transmissioninfo.split()[-1]))
+            return False
+    return False
+
+def check_all(watchlocal=set(), watchremote=set(), lockfilelist=[], transmissioncommand="", transmissionminimalspeed=0.0):
+    # this routine checks if the system is active. If it finds an activity, it will return True and not check other activities.
+    if lockfilelist:
+        if check_lockfiles(lockfilelist):
+            return True
+    if watchlocal or watchremote:
+        if check_services(watchlocal,watchremote):
+            return True
+    if transmissioncommand:
+        if check_transmission(transmissioncommand, transmissionminimalspeed):
+            return True
+    return False
+
+def getSettings():
+    global sleep_time, watched_local, watched_remote, counterdefault, addon
+    global lockfilepaths, transmisionMinimalSpeed , transmissionCmd, debugMe
+    
+    addon = xbmcaddon.Addon()
+    s = addon.getSetting
+    try:
+        sleep_time = int(s('sleep'))
+    except ValueError:
+        sleep_time = 60
+    watched_local = port_set(s('localports'))
+    watched_remote = port_set(s('remoteports'))
+    counterdefault = int(s('idlecount'))
+    lockfilepaths=s('lockfilepaths')
+    if lockfilepaths:
+        lockfilepaths=lockfilepaths.split(";")
+    else:
+        lockfilepaths=[]
+    checkTransmission = s('checktransmission') == 'true'
+    transmisionMinimalSpeed = float(s('transmissionminspeed'))
+    transmissionUsername = s('transmissionuser')
+    transmissionPasswd = s('transmissionpass')
+    if not checkTransmission:
+        transmissionCmd = ""
+    elif transmissionUsername and transmissionPasswd:
+        transmissionCmd = "transmission-remote -n %s:%s -l" %(transmissionUsername, transmissionPasswd)
+    else:
+        transmissionCmd = "transmission-remote -l"
+    debugMe = s('debugme') == 'true'
+    #FIXME: I need to check if transmission-remote always returns kb/s
+
+getSettings()
 counter = counterdefault
 
-log("Watching for remote connections to ports {} and for local connections to ports {}, sleep time is {:0.2f} s.".format(
+mylog("Watching for remote connections to ports {} and for local connections to ports {}, sleep time is {} s.".format(
     ', '.join(str(x) for x in watched_remote),
     ', '.join(str(x) for x in watched_local),
-    sleep_time / 1000.0))
+    sleep_time))
 
 while not xbmc.abortRequested:
-    if check_services(watched_local, watched_remote):
-        xbmc.executebuiltin('InhibitIdleShutdown(true)')
-        counter = counterdefault
-    elif counter > 0: # if we have a value on the counter, we leave shutdown as it is.
-        counter = counter -1
-        log("Delay counter is: %i. Not changing InhibitIdleShutdown.")
-    else:
-        log("Setting InhibitIdleShutdown to false")
-        xbmc.executebuiltin('InhibitIdleShutdown(false)')
-    xbmc.sleep(sleep_time)
+    for i in range(sleep_time):
+        if xbmc.abortRequested: break
+        getSettings()
+        if i==0:
+            if check_all(watched_local, watched_remote, lockfilepaths, transmissionCmd, transmisionMinimalSpeed):
+                mylog("Setting InhibitIdleShutdown to true")
+                xbmc.executebuiltin('InhibitIdleShutdown(true)')
+                counter = counterdefault
+            elif counter > 0: # if we have a value on the counter, we leave shutdown as it is.
+                counter = counter -1
+                mylog("Delay counter is: %i. Not changing InhibitIdleShutdown." %(counter))
+            else:
+                mylog("Setting InhibitIdleShutdown to false")
+                xbmc.executebuiltin('InhibitIdleShutdown(false)')
+        xbmc.sleep(1000)
